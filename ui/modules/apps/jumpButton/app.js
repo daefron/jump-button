8fa930163931decf8e4e961c01a18b33d3e8ea51
Thomas Evans <thomas_evans@outlook.com>
2025-03-11 13:21:55 +1100

    Added charge bar effect to ui

--- ui/modules/apps/jumpButton/app.js
+++ ui/modules/apps/jumpButton/app.js
@@ -8,10 +8,11 @@ angular.module("beamng.apps").directive("jumpButton", [
       link: function (scope, element, attrs) {
         scope.jumpStrength = 100;
         scope.cooldown = {
-          maxTime: 1000,
-          waiting: false,
+          maxTime: 100,
           jumpReady: "Ready",
           color: { color: "rgb(0,255,0)" },
+          boxShadow:
+            "inset 5000px 0px rgba(0, 255, 0, 0.2)",
         };
 
         // used when user presses jump key
@@ -27,11 +28,31 @@ angular.module("beamng.apps").directive("jumpButton", [
           scope.cooldown.jumpReady = "Not Ready";
           scope.cooldown.color = { color: "rgb(255,0,0)" };
 
-          setTimeout(() => {
-            scope.cooldown.waiting = false;
-            scope.cooldown.jumpReady = "Ready";
-            scope.cooldown.color = { color: "rgb(0,255,0)" };
-          }, scope.cooldown.maxTime);
+          const windowWidth = document.getElementById("jumpButton").offsetWidth;
+          scope.cooldown.boxShadow = "inset 0px 0px rgba(50, 50, 50, 0.2);";
+          scope.cooldown.currentTime = 0;
+          const readyLoop = setInterval(() => {
+            scope.$applyAsync(function () {
+              if (scope.cooldown.currentTime >= scope.cooldown.maxTime) {
+                scope.cooldown.waiting = false;
+                scope.cooldown.jumpReady = "Ready";
+                scope.cooldown.color = { color: "rgb(0,255,0)" };
+                clearInterval(readyLoop);
+              }
+              scope.cooldown.currentTime++;
+              const percentage =
+                scope.cooldown.currentTime / scope.cooldown.maxTime;
+              const boxShadowValue = percentage * windowWidth;
+              const boxShadowColor =
+                (1 - percentage) * 255 + ", " + percentage * 255;
+              scope.cooldown.boxShadow =
+                "inset " +
+                boxShadowValue +
+                "px 0px rgba(" +
+                boxShadowColor +
+                ", 50, 0.2)";
+            });
+          }, 10);
 
           bngApi.activeObjectLua("obj:setGravity(" + scope.jumpStrength + ")");
           setTimeout(() => {
